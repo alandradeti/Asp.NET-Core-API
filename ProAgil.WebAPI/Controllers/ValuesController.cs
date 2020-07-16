2a2350637416f2870a3176393d24ff852c72b97d
Ailton Lima <ailtonlm.andrade@outlook.com>
2020-07-16 13:31:04 -0300

    API inicial projeto de Eventos

---
+++ ProAgil.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProAgil.WebAPI.Model;
+using ProAgil.WebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+
+        public readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //GET Default route without parameter
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _context.Events.ToListAsync();
+                
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failed");
+            }
+        }
+
+        //GET Route with parameter pass "Id" to be informed in the url
+        [HttpGet ("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            try
+            {
+                var results = await _context.Events.FirstOrDefaultAsync(x => x.EventId == id);;
+                
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failed");
+            }
+        }
+    }
+}
--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Data;
+
 
 namespace ProAgil.WebAPI
 {
@@ -25,6 +28,10 @@ namespace ProAgil.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>
+            (
+                x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
+            );
             services.AddControllers();
         }
 
@@ -36,7 +43,7 @@ namespace ProAgil.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
